568fb32c062de0ab775db0391bc36efe4e684d33
SGERASIMOV <sgerasimov@griddynamics.com>
2017-12-26 15:24:18 +0200

    negative value in form bug fix

--- app/v1/transaction/forms/TransactionControllerForms.scala
+++ app/v1/transaction/forms/TransactionControllerForms.scala
@@ -8,10 +8,14 @@ trait TransactionControllerForms {
 
   import play.api.data.Forms._
 
+  private def isAmountPositive(changeAmount: BigDecimal): Boolean = {
+    changeAmount >= 0
+  }
+
   val form = Form(
     mapping(
       "actionType" -> nonEmptyText,
-      "changeAmount" -> bigDecimal
+      "changeAmount" -> bigDecimal.verifying("amount is less than 0", isAmountPositive _)
     )(TransactionFormInput.apply)(TransactionFormInput.unapply)
   )
 }
